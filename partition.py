12908bfe7e0dc655cc91beeebfb195cfa1cb23e4
Gabriel Pizarro <gpizarro@javaman.net>
2020-05-23 22:48:19 -0700

    Script with all features complete

--- partition.py
+++ partition.py
@@ -1,8 +1,10 @@
 # Finds all the ways to add up to a number with other numbers!
 # Uses a recursive, brute-force function
+# Loads simple parameters from cli
+# If min_n, max_n, repeat_allowed, or disallowed need to be specified,
+#     write a script that uses partition() outside of this file
 
-
-def partition(num, length, repeat_allowed=False, disallowed=[]):
+def partition(num, length, min_n=1, max_n=9, repeat_allowed=False, disallowed=[]):
     results = []
 
     if length == 1 and num <= 9 and num >= 1:
@@ -11,19 +13,16 @@ def partition(num, length, repeat_allowed=False, disallowed=[]):
     else:
         if not repeat_allowed:
             disallowed.append(num)
-        for i in range(1, 10):
-            if i not in disallowed:
+        for i in range(min_n, max_n + 1):
+            oob_issue = length - 1 == 1 and \
+                num - i not in range(min_n, max_n + 1)
+            if i not in disallowed and not oob_issue:
                 sub_results = partition(
-                    num - i, length - 1, repeat_allowed, disallowed.copy())
-                if len(sub_results) > 0:
-                    break
+                    num - i, length - 1, min_n, max_n, repeat_allowed, disallowed.copy())
                 for sub_result in sub_results:
                     if repeat_allowed or i not in sub_result:
                         sub_result.append(i)
-                        total = 0
-                        for j in sub_result:
-                            total += j
-                        if total == num:
+                        if sum(sub_result) == num:
                             sub_result.sort()
                             if sub_result not in results:
                                 results.append(sub_result)
